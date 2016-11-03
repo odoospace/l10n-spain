61c7e7be9529215bec64493506fa9bad27c9eda4
Javier Colmeiro <javir.colmeiro@impulzia.com>
2016-11-03 13:50:03 +0100

    + add minimal changes to payment_redsys to make it installable and configurable need review for usable

--- payment_redsys/__openerp__.py
+++ payment_redsys/__openerp__.py
@@ -4,12 +4,12 @@
     'name': 'Redsys Payment Acquirer',
     'category': 'Hidden',
     'summary': 'Payment Acquirer: Redsys Implementation',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'author': "Incaser Informatica S.L.,Odoo Community Association (OCA)",
     'depends': ['payment'],
     'data': [
         'views/redsys.xml',
         'views/payment_acquirer.xml'
     ],
-    'installable': False,
+    'installable': True,
 }
